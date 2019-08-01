8e464ab40dbaa653d1fe2fb89eb08846fe9f7801
YeonSungMin <meisensei0312@hotmail.com>
2019-08-02 00:04:39 +0900

    Add EnemyTypePredict in AI.h , AI.cpp Move EnemyTypeChase in AI.h , AI.cpp Add struct Relativity in Input.h

--- ShadowMan/AI.h
+++ ShadowMan/AI.h
@@ -1,15 +1,28 @@
 #include <math.h>
 #include "input.h"
 
+void EnemyTypeChase();  // 追跡型の敵
 
-//　先読み型の敵
-void ContectTime(); // 接近時間を算出する関数
 
+void EnemyTypePredict();// 先読み型の敵
+
+	// プレイヤーの移動量(Speed) - 敵の移動量(Speed)　= Vr (相対速度)
+	// プレイヤー座標 - 敵座標 = Sr (相対距離)
+	// Tc(接近時間) = |Sr| / |Vr|  これで、敵がプレイヤーの座標を予測
+
+
+void EnemyTypeFlee(); // 逃走型の敵
 /*
 
- Player Speed - Enemy Speed = Vr (相対速度)
- Player Position - Enemy Position = Sr (相対距離)
- Tc(接近時間) = |Sr| / |Vr|
+ プレイヤーの座標 + 敵座標　= とある座標点
+
+ ||とある座標|| * 移動量
 
 */
 
+void EnemyTypePatrol(); // 巡回型の敵
+/*
+
+
+
+*/
